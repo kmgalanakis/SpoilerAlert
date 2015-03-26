a7414a7aa3ca1a326ea436f64c64a87ff261cc38
kmgalanakis <kmgalanakis@gmail.com>
2015-03-26 11:22:09 +0200

    Remove redundant folder created by mistake

--- ${gitrepo}/js/script.js
+++
@@ -1,9 +0,0 @@
-/* #-------------------------------------------------------------------------- 
- # Spoiler Alert Content Plugin 
- # ------------------------------------------------------------------------- 
- # version   0.2 
- # date      26 March 2015 
- # author    Konstantinos Galanakis - https://github.com/kmgalanakis 
- # copyright Copyright (C) 2015 Konstantinos Galanakis. All Rights Reserved 
- # license   GNU/GPL license v2: https://www.gnu.org/licenses/gpl-2.0.html 
- #--------------------------------------------------------------------------*/
